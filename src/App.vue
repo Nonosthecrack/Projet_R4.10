9a635fc3cae63325a7327b028b65ec76378114b9
Noa-f <ferault.noa@gmail.com>
2025-02-25 09:59:56 +0100

    ajout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
-import { ref } from "vue";
-const text = ref("Hello world");
+import { computed, ref } from "vue";
+const text = ref("");
+
+const trimmedText = computed(() => text.value.trim());
+
 const posts = ref([]);
 
 function addPost() {
-  posts.value.push(text.value);
+  const newPost = {
+    id: Math.random().toString(36).substring(2),
+    content: trimmedText.value,
+    createdAt: new Date(),
+    author: {
+      username: "Nono",
+      avatarUrl:
+        "https://media.licdn.com/dms/image/v2/C4D03AQH0TuKfHrOpfQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1607679351861?e=2147483647&v=beta&t=DQp5RzUGjvkvOkY3dF-EieiqoIV6RVdKdJrpnvaOrpM",
+    },
+  };
+  posts.value.push(newPost);
   text.value = "";
 }
 </script>
@@ -13,10 +26,31 @@ function addPost() {
   <main>
     <div class="container">
       <form class="card" @submit.prevent="addPost">
-        <textarea name="post" id="post" placeholder="Quoi de neuf ?" v-model="text"></textarea>
-        <button type="submit">Publier</button>
+        <textarea
+          name="post"
+          id="post"
+          placeholder="Quoi de neuf ?"
+          maxlength="100"
+          v-model="text"
+        ></textarea>
+        <button type="submit" :disabled="!trimmedText.length">Publier</button>
       </form>
-      <p v-for="(post, index) in posts" :key="index">{{ post }}</p>
+
+      <p v-if="!posts.length">Pas de post pour le moment</p>
+
+      <article v-for="(post, index) in posts" :key="index" class="card">
+        <div class="post-header">
+          <img
+            :src="post.author.avatarUrl"
+            alt="pdp"
+            width="50px"
+            height="50px"
+            class="user-img"
+          />
+          <a>{{ post.author.username }}</a>
+        </div>
+        <p>{{ post.content }}</p>
+      </article>
     </div>
   </main>
 </template>
@@ -62,5 +96,31 @@ button {
   font-size: 1rem;
   height: 40px;
   padding: 0 1rem;
+}
+
+button:disabled {
+  cursor: not-allowed;
+  opacity: 0.4;
+}
+
+article {
+  padding: 0.75rem 1.5rem;
+  overflow: hidden;
+}
+
+article p {
+  white-space: pre-wrap;
+}
+
+.user-img {
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.post-header {
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  margin-bottom: 0.75rem;
 }
 </style>
